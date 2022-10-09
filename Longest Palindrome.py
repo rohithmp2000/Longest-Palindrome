13e6485d6d227130fea54b0fee5c4e67ce16fff3
Rohith M P <rohithmp2014@gmail.com>
2022-10-10 00:28:45 +0530

    Task Completed.

---
+++ Longest Palindrome.py
@@ -0,0 +1,16 @@
+# Program to print the longest string in a palindrome
+
+def longestPalindrome():
+    s = str(input("Enter a string: "))    
+    palindrome = ''
+    for i in range(len(s)):
+        for j in range(len(s), i, -1):
+            if len(palindrome) >= j-i:
+                break
+            elif s[i:j] == s[i:j][::-1]:
+                palindrome = s[i:j]
+                break
+    return palindrome
+
+result=longestPalindrome()
+print(result)
